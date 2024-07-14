fa4cb1ab26aa41c75f2190664e78c0a96c860288
Gespenct <18egor200202@gmail.com>
2024-07-14 13:29:13 +0500

    KS-1-3 create vars

---
+++ src/main/kotlin/lesson_1/lesson1_task3.kt
@@ -0,0 +1,10 @@
+package org.example.lesson_1
+
+fun main() {
+    val year: Int = 1961
+    var hour: String = "09"
+    var minute: String = "07"
+    println(year)
+    println(hour)
+    println(minute)
+}
