61facc14f1fc30960fc4afbf89a36c31a2b7344d
Gespenct <18egor200202@gmail.com>
2024-07-14 14:30:22 +0500

    KS-1-5 create vars

---
+++ src/main/kotlin/lesson_1/lesson1-task5.kt
@@ -0,0 +1,11 @@
+package org.example.lesson_1
+
+fun main() {
+    val second1: Short = 6480
+
+    val hour: String = "01"
+    val minute: Byte = 48
+    val second2: String = "00"
+
+    println("$hour:$minute:$second2")
+}
