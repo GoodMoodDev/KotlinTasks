14ebd4896b739b1712a257e0f2cd5e26df0f0a82
Gespenct <18egor200202@gmail.com>
2024-07-14 12:27:50 +0500

    KS-1-2 create vars

---
+++ src/main/kotlin/lesson_1/lesson1_task2.kt
@@ -0,0 +1,14 @@
+package org.example.lesson_1
+
+fun main() {
+    val numberOfOrders: Int = 75
+    var thanksToTheBuyer: String = "Спасибо за покупку!"
+    println(numberOfOrders)
+    println(thanksToTheBuyer)
+
+    var amountOfWorkers: Int = 2000
+//    println(amountOfWorkers)
+
+    amountOfWorkers = 1999
+    println(amountOfWorkers)
+}
