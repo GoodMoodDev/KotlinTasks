8b5d5ddc3c976af76bceee2160a32019f0a46f04
Gespenct <18egor200202@gmail.com>
2024-07-15 14:49:04 +0500

    KS-1-5 making corrections

--- src/main/kotlin/lesson_1/lesson1-task5.kt
+++ src/main/kotlin/lesson_1/lesson1-task5.kt
@@ -1,11 +1,12 @@
 package org.example.lesson_1
 
 fun main() {
-    val second1: Short = 6480
+    var second1: Short = 6480
 
-    val hour: String = "01"
-    val minute: Byte = 48
-    val second2: String = "00"
+    var hour: Int = second1 / 3600
+    var minute1: Int = second1 - 3600
+    var minute2: Int = minute1 / 60
+    var second2: Int = minute2 * 60 - minute1
 
-    println("$hour:$minute:$second2")
+    println("0$hour:$minute2:0$second2")
 }
