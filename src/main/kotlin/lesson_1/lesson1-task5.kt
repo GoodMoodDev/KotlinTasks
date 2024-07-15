ffd86c336103320930e76269fcc5099c1d162b84
Gespenct <18egor200202@gmail.com>
2024-07-15 22:04:16 +0500

    KS-1-5 code optimization

--- src/main/kotlin/lesson_1/lesson1-task5.kt
+++ src/main/kotlin/lesson_1/lesson1-task5.kt
@@ -1,12 +1,11 @@
 package org.example.lesson_1
 
 fun main() {
-    var second1: Short = 6480
-
-    var hour: Int = second1 / 3600
-    var minute1: Int = second1 - 3600
-    var minute2: Int = minute1 / 60
-    var second2: Int = minute2 * 60 - minute1
-
-    println("0$hour:$minute2:0$second2")
+    fun timeSecondsToStr(seconds: Int = 6480): String {
+        val hour = seconds / 3600
+        val minute = (seconds % 3600) / 60
+        val second = seconds % 60
+        return String.format("%02d:%02d:%02d", hour, minute, second)
+    }
+    println(timeSecondsToStr())
 }
