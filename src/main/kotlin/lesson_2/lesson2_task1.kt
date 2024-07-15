650c53e58b961d25c458e2be8660468322ea71d5
Gespenct <18egor200202@gmail.com>
2024-07-15 15:26:29 +0500

    KS-2-1 create algorithm

---
+++ src/main/kotlin/lesson_2/lesson2_task1.kt
@@ -0,0 +1,13 @@
+package org.example.lesson_2
+
+fun main() {
+    var a: Byte = 3
+    var b: Byte = 4
+    var c: Byte = 3
+    var d: Byte = 5
+
+    var count1: Int = a + b + c + d
+    var count2: Float = count1 / 4f
+
+    println(count2)
+}
