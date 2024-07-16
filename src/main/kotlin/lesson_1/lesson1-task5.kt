0c70e4b090780fdb0eab69181a5cd0788739c4cf
Gespenct <18egor200202@gmail.com>
2024-07-16 18:29:52 +0500

    KS-1-5 add perHour / perMinute / perSecond vars

--- src/main/kotlin/lesson_1/lesson1-task5.kt
+++ src/main/kotlin/lesson_1/lesson1-task5.kt
@@ -1,10 +1,14 @@
 package org.example.lesson_1
 
 fun main() {
+
     fun timeSecondsToStr(seconds: Int = 6480): String {
-        val hour = seconds / 3600
-        val minute = (seconds % 3600) / 60
-        val second = seconds % 60
+        val secondsPerHour: Short = 3600
+        val minutesPerHour: Byte = 60
+        val secondsPerMinute: Byte = 60
+        val hour: Int = seconds / secondsPerHour
+        val minute: Int = (seconds % secondsPerHour) / minutesPerHour
+        val second: Int = seconds % secondsPerMinute
         return String.format("%02d:%02d:%02d", hour, minute, second)
     }
     println(timeSecondsToStr())
