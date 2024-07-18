3a7124afdc3855fd7faed57324e98880f0d1c778
Gespenct <18egor200202@gmail.com>
2024-07-18 20:09:29 +0500

    KS-3-1 create a greeting message

---
+++ src/main/kotlin/lesson_3/lesson3_task1.kt
@@ -0,0 +1,12 @@
+package org.example.lesson_3
+
+fun main() {
+    var greeting: String = "Добрый день"
+    val userName: String = "Георгий"
+
+    println("$greeting, $userName!")
+
+    greeting = "Добрый вечер"
+
+    println("$greeting, $userName!")
+}
